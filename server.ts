70129298f2757ee7bfecb5f73bdcae61404be84f
joshhunt <josh@trtr.co>
2025-02-01 12:30:23 +0000

    handlebars templates

--- server.ts
+++ server.ts
@@ -1,15 +1,9 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import cookieParser from "cookie-parser";
-import {
-  fetchMovieData,
-  type TheaterScreenings,
-  type MovieScreenings,
-  type DayScreenings,
-  type Screening,
-  type ScreeningsQuery,
-} from "./data.ts";
+import { fetchMovieData, type ScreeningsQuery } from "./data.ts";
 import { watch } from "fs";
 import { readFile } from "fs/promises";
+import { create } from "express-handlebars";
 import {
   DEFAULT_DATE_START,
   DEFAULT_DATE_END,
@@ -19,9 +13,22 @@ import {
 const app = express();
 app.use(cookieParser());
 
+const hbs = create({
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    space(formattedTime: string) {
+      return " ".repeat("12:00 pm".length - formattedTime.length);
+    },
+  },
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
 const port = process.env.PORT || 3000;
 
-const CSS_PATH = "styles.css";
+const CSS_PATH = "./views/styles.css";
 let css = await readFile(CSS_PATH, "utf-8");
 watch(CSS_PATH, async (eventType, path) => {
   if (eventType === "change" && path) {
@@ -29,38 +36,50 @@ watch(CSS_PATH, async (eventType, path) => {
   }
 });
 
-app.get("/", async (req, res) => {
-  // Get existing cookie value or initialize empty array
+function getSeenMovieIds(req: Request) {
   const existingCookie: string = req.cookies?.seenMovieIds || "";
-  let seenMovieIds = existingCookie ? existingCookie.split(",") : [];
+  return existingCookie ? existingCookie.split(",") : [];
+}
 
-  console.log({ seenMovieIds });
+app.get("/set-seen", (req, res) => {
+  let seenMovieIds = getSeenMovieIds(req);
 
-  const seenMovieId = req.query.seen;
-  if (seenMovieId) {
-    // Add new ID if not already present
-    if (!seenMovieIds.includes(String(seenMovieId))) {
-      seenMovieIds.push(String(seenMovieId));
-    } else {
-      seenMovieIds = seenMovieIds.filter((id) => id !== String(seenMovieId));
-    }
-
-    // Set updated cookie
-    res.cookie("seenMovieIds", seenMovieIds.join(","), {
-      maxAge: 365 * 24 * 60 * 60 * 1000, // 1 year
-      httpOnly: true,
-    });
-
-    // Redirect back to homepage
-    return res.redirect("/");
+  const movieId = req.query.movieId;
+  if (!movieId) {
+    res.status(400).send("No movie ID provided");
+    return;
   }
 
-  try {
+  // Add new ID if not already present
+  if (!seenMovieIds.includes(String(movieId))) {
+    seenMovieIds.push(String(movieId));
+  } else {
+    seenMovieIds = seenMovieIds.filter((id) => id !== String(movieId));
+  }
+
+  // Set updated cookie
+  res.cookie("seenMovieIds", seenMovieIds.join(","), {
+    maxAge: 365 * 24 * 60 * 60 * 1000, // 1 year
+    httpOnly: true,
+  });
+
+  // Redirect back to homepage
+  res.redirect("/");
+  return;
+});
+
+app.get(
+  "/",
+  wrapErrors(async (req, res) => {
+    const seenMovieIds = getSeenMovieIds(req);
+
     const query: ScreeningsQuery = {
       fromDate: DEFAULT_DATE_START,
       toDate: DEFAULT_DATE_END,
       theaters: DEFAULT_THEATRED_RANKED,
     };
+
+    let dfdf: Boolean = true;
 
     const movieData = await fetchMovieData(query);
 
@@ -80,73 +99,27 @@ app.get("/", async (req, res) => {
       };
     });
 
-    let html = `<html><head><title>Everyman Movie Listings</title>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <style>
-      ${css}
-    </style></head><body>`;
-
-    const maxTimes = movieData
-      .flatMap((theater) =>
-        theater.movies.flatMap((movie) =>
-          movie.days.map((day) => day.screenings.length)
-        )
-      )
-      .reduce((max, time) => Math.max(max, time), 0);
-
-    const renderScreening = (screening: Screening) => {
-      const paddingSpaces = " ".repeat(
-        "12:00 pm".length - screening.formattedTime.length
-      );
-      return `<td class="pre-text">${paddingSpaces}<a href="${screening.url}">${screening.formattedTime}</a></td>`;
-    };
-
-    const renderDay = (day: DayScreenings) => `
-                <tr class="screenings">
-                  <td>${day.formattedDate}</td> ${day.screenings
-      .map(renderScreening)
-      .join(" ")}
-                </tr>
-              `;
-
-    const renderMovie = (movie: MovieScreenings) => {
-      return `
-            <div class="movie">
-              <details ${movie.seen ? "" : "open"}>
-                <summary><h3><a href="https://www.everymancinema.com${
-                  movie.path
-                }">${movie.title}</a> <a class="seen-link" href="?seen=${
-        movie.movieId
-      }">[${movie.seen ? "unsee" : "seen"}]</a></h3></summary>
-                <table>
-                  <tbody>
-                    ${movie.days.map(renderDay).join("")}
-                  </tbody>
-                </table>
-              </details>
-            </div>
-          `;
-    };
-
-    const renderTheater = (theaterData: TheaterScreenings) => `
-        <div class="theater">
-          <h2>${theaterData.theaterName}</h2>
-          ${theaterData.movies.map(renderMovie).join("")}
-        </div>
-      `;
-
-    html += `
-      ${processedMovieData.map(renderTheater).join("")}
-    `;
-
-    html += "</body></html>";
-    res.send(html);
-  } catch (error: any) {
-    console.error(error);
-    res.status(500).send(`Error: ${error.message}`);
-  }
-});
+    res.render("movieListings", {
+      css,
+      theaters: processedMovieData,
+    });
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+function wrapErrors(fn: (req: Request, res: Response) => Promise<void>) {
+  return async (req: Request, res: Response) => {
+    try {
+      await fn(req, res);
+    } catch (error: any) {
+      console.error(error);
+      res
+        .status(500)
+        .contentType("text/plain")
+        .send(`Error: ${error.message ?? error.toString()}`);
+    }
+  };
+}
